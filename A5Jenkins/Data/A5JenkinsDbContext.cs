826d608c543a04383016daae4b18a9336aeaf6c7
keerthana <keerthananeeluru2001@gmail.com>
2023-09-14 14:37:58 +0530

    commit

---
+++ A5Jenkins/Data/A5JenkinsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using A5Jenkins.Model;
+
+namespace A5Jenkins.Data
+{
+    public class A5JenkinsDbContext : DbContext
+    {
+        public A5JenkinsDbContext (DbContextOptions<A5JenkinsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<A5Jenkins.Model.User> User { get; set; } = default!;
+    }
+}
---
+++ A5Jenkins/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using A5Jenkins.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<A5JenkinsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("A5JenkinsDbContext") ?? throw new InvalidOperationException("Connection string 'A5JenkinsDbContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
